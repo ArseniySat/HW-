a3d9bcc7a82a93a591d555148948ac5f143396a6
Арсений Сат <nsat8icloud.com@MacBook-Pro-Nadezda-2.local>
2026-05-23 14:47:06 +0300

    Add color script

---
+++ script.js
@@ -0,0 +1,6 @@
+const a = Math.random();
+const b = Math.random();
+const sum = a + b;
+
+// \x1b[33m — жёлтый цвет, \x1b[0m — сброс цвета
+console.log(`\x1b[33mСумма двух случайных чисел: ${sum}\x1b[0m`);
