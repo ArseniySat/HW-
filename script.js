8a5cbfe1438381a86b1570136b9c791f2be1021d
Арсений Сат <nsat8icloud.com@MacBook-Pro-Nadezda-2.local>
2026-05-23 15:25:22 +0300

    Update output message

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ const a = Math.random();
 const b = Math.random();
 const sum = a + b;
 
-console.log(`\x1b[33mСумма двух случайных чисел: ${sum}\x1b[0m`);
+console.log(chalk.yellow(`Результат сложения: ${a} + ${b} = ${sum}`));
